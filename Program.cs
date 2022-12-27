7557d259b159b899b81d83530ccbc1cddff88add
RokiWielki <Cruck222@interia.pl>
2022-12-27 11:36:43 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rectangles
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n, cnt = 0;
+            n= int.Parse(Console.ReadLine());
+            for (int i = 1; i <=n; i++)
+            {
+                for (int j = i; j <=n; j++)
+                {
+                    if (i*j<=n)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
